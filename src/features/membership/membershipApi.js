2cc22077701878972b5aeb954f0cbabc33ba1155
PG47 <86549123+PG47@users.noreply.github.com>
2024-05-04 23:23:14 +0700

    membership API

--- src/features/membership/membershipApi.js
+++ src/features/membership/membershipApi.js
@@ -1,1 +1,51 @@
 // call API here
+import axios from "../../config/axiosConfig";
+import {slice} from "./membershipSlice";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export const request_membership = async (user, dispatch, navigate, id) => {
+    dispatch(slice.requestBegin())
+    try{
+        const res = await axios.post('/membership/user/request-membership/' + id, {
+            ...user,
+        }, {
+            'Content-Type': 'application/json'
+        })
+        dispatch(slice.requestSuccess(res.data))
+        toast.success('Request successful!');
+        // Navigate to payment process here, depending on the payment method
+        if (res.data.paymentMethod === 'paypal') {
+            // Redirect to PayPal payment page
+            window.location.href = 'https://www.paypal.com/payment_page';
+        } else if (res.data.paymentMethod === 'momo') {
+            // Redirect to Momo payment page
+            window.location.href = 'https://www.momo.com/payment_page';
+        } else {
+            // If payment method is not specified or unsupported, navigate to myaccount page
+            navigate('/myaccount');
+        }
+    } catch (err){
+        console.log(err.response);
+        dispatch(slice.requestFailure());
+        toast.error(err.response.data);
+    }
+}
+
+export const update_membership = async (user, dispatch, navigate, id) => {
+    dispatch(slice.updateBegin())
+    try{
+        const res = await axios.post('/membership/user/update-membership/' + id, {
+            ...user,
+        }, {
+            'Content-Type': 'application/json'
+        })
+        dispatch(slice.updateSuccess(res.data))
+        toast.success('Update membership successful!');
+        navigate('/myaccount');
+    } catch (err){
+        console.log(err.response);
+        dispatch(slice.updateFailure);
+        toast.error(err.response.data);
+    }
+}
--- src/features/membership/membershipSlice.js
+++ src/features/membership/membershipSlice.js
@@ -1,1 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    currentUser: null,
+
+    request: {
+        isFetching: false,
+        error: false
+    },
+
+    update: {
+        isFetching: false,
+        error: false
+    }
+}
+
+const membershipSlice = createSlice({
+    name: "membership",
+    initialState,
+    reducers: {
+        //request
+        requestBegin: (state) => {
+            state.request.isFetching = true
+        },
+
+        requestSuccess: (state, action) => {
+            state.request.isFetching = false
+            state.currentUser = action.payload
+            state.request.error = false
+        },
+
+        requestFailure: (state) => {
+            state.request.isFetching = false
+            state.request.error = true
+        },
+
+
+        //Update
+        updateBegin: (state) => {
+            state.update.isFetching = true
+        },
+
+        updateSuccess: (state, action) => {
+            state.update.isFetching = false
+            state.currentUser.user = action.payload
+            state.update.error = false
+        },
+
+        updateFailure: (state) => {
+            state.update.isFetching = false
+            state.update.error = true
+        }
+    }
+})
+
+export const slice = membershipSlice.actions
+export default membershipSlice.reducer
